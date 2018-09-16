08381684e5defb989f995090af37eb3bd2281c4d
Kunal Dubey <kunaldubey2297@gmail.com>
2018-09-17 01:00:57 +0530

    verbal screening changes

--- server.js
+++ server.js
@@ -18,14 +18,25 @@ const knex = require('knex')({
 
 app.post('/verbal-screening',urlencodedParser,(req,res)=>{   
 
-	const {state, district ,town , facility }=req.body;
+	const {state, district ,town , facility  }=req.body;
 	knex('verbal_screening').insert({  
 		state : state, 
 		district: district ,
 		town: town , 
-		facility: facility,
+		facility: facility, 
+		children_attended_opd:req.body.children_attended_opd,
+		children_screened_for_tb_tool:req.body.children_screened_for_tb_tool,
 
-	}).then(console.log  )
+	}).then(console.log  ) 
+	.then(console.log("Form data uploaded")) 
+	.catch(err=>res.send("Form data upload unsuccessful")) 
+	if(req.body.children_attended_opd>5){ 
+	res.status(200).send("Form data has been uploaded");  
+	}  
+	else {  
+		res.status(400).send("Error uploading form data");
+	}
+	
 });
 // app.post('/tb-treatment',(req,res)=>{  
 
